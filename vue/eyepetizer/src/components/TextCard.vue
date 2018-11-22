e351b99232e67357351ddacbcfc1480611bb388e
yueYueNiao <33487880+chenzp1996@users.noreply.github.com>
2018-11-23 01:00:26 +0800

    Add files via upload

---
+++ vue/eyepetizer/src/components/TextCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="textCard">
+    <h1 class="text" >{{item.data.text}}<i class="el-icon-arrow-right"></i></h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "textCard",
+  data:function(){
+    return {
+    //   text:this.$parent.$data.textCard
+    }
+  },
+  props: ["item"]
+};
+</script>
+
+<style lang="less" scoped>
+.textCard {
+  .text {
+    border-top:1px solid #EEE;
+    padding-top: 20px;
+    margin-top: 20px;
+    font-size: 18px;
+    font-weight: bold;
+    line-height: 20px;
+  }
+}
+</style>
